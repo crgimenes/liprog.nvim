502bea9b41d00629c65fd0dfe142847fa961a650
crgimenes <crg@crg.eti.br>
2023-01-31 10:38:31 -0300

    get current line

--- lua/liprog/init.lua
+++ lua/liprog/init.lua
@@ -6,4 +6,17 @@ M.setup = function(opts)
     vim.g.liprog_settings = opts or default_config
 end
 
+-- get current line number
+M.get_current_line_number = function()
+    local line = vim.api.nvim_win_get_cursor(0)[1]
+    return line
+end
+
+-- get line by line number
+M.get_line = function(line_number)
+    local line = vim.api.nvim_buf_get_lines(0, line_number - 1, line_number, false)[1]
+    return line
+end
+
+
 return M
